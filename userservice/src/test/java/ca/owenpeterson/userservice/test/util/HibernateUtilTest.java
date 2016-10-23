2f603540606014254299ab4cf996a98441584b2d
Owen Peterson <owen.a.peterson@gmail.com>
2016-10-23 14:13:21 -0500

    Added another test

--- userservice/src/test/java/ca/owenpeterson/userservice/test/util/HibernateUtilTest.java
+++ userservice/src/test/java/ca/owenpeterson/userservice/test/util/HibernateUtilTest.java
@@ -2,6 +2,7 @@ package ca.owenpeterson.userservice.test.util;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 
@@ -14,5 +15,13 @@ public class HibernateUtilTest {
 	{
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		assertNotNull("sessionFactory should not be null! Did you create the database? Is it running?", sessionFactory);
+	}
+	
+	@Test
+	public void testGetSession()
+	{
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		assertNotNull("Session should not be null!", session);
 	}
 }
