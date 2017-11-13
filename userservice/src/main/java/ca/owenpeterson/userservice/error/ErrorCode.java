fa5eaff7a128fc3c17813d836f3a4b7480fc731b
Owen <Owen@WIN10-DESKTOP.hitronhub.home>
2017-11-13 12:16:15 -0600

    ErrorCode properties are now displaying properly in JSON format, but not XML.

--- userservice/src/main/java/ca/owenpeterson/userservice/error/ErrorCode.java
+++ userservice/src/main/java/ca/owenpeterson/userservice/error/ErrorCode.java
@@ -2,6 +2,13 @@ package ca.owenpeterson.userservice.error;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@XmlRootElement(name="error_code")
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode implements Serializable
 {
 	USERNAME_EXISTS("1001", "The specified username already exists."),
@@ -16,13 +23,15 @@ public enum ErrorCode implements Serializable
 		this.message = message;
 	}
 
+	@XmlElement(name="code")
 	public String getCode() 
 	{
 		return code;
 	}
 
+	@XmlElement(name="message")
 	public String getMessage() 
 	{
 		return message;
-	}	
+	}
 }
