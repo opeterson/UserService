2ee117b3188c8a5b3259d585e7b0f52f4f422828
Owen <Owen@WIN10-DESKTOP.hitronhub.home>
2017-11-25 15:13:34 -0600

    Removed Mockito dependency. Added Easymock dependency.

--- userservice/src/test/java/ca/owenpeterson/userservice/test/util/HibernateUtilTest.java
+++ userservice/src/test/java/ca/owenpeterson/userservice/test/util/HibernateUtilTest.java
@@ -8,7 +8,8 @@ import org.junit.Test;
 
 import ca.owenpeterson.userservice.persistence.HibernateUtil;
 
-public class HibernateUtilTest {
+public class HibernateUtilTest 
+{
 	
 	@Test
 	public void testGetSessionFactory()
